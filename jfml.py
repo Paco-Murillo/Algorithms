f0428eb19fe68cfabefba5447142b88b0b9504db
A01377744 <A01377744@itesm.mx>
2020-05-02 15:26:15 -0500

    Ksat

--- jfml.py
+++ jfml.py
@@ -11,7 +11,13 @@ def insertionSort(list):
 
 
 def bubbleSort(list):
-    pass
+    for i in range(1, len(list)):
+        for j in range(0, len(list) - i):
+            if list[j] > list[j + 1]:
+                temp = list[j]
+                list[j] = list[j + 1]
+                list[j + 1] = temp
+            print(list)
 
 
 def divide(list, first, last):
